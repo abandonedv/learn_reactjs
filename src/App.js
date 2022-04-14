418198a6528643b2ad7e5530398ab0962c894c18
Вадим Ильиных <vadimiiofficial@mail.ru>
2022-04-15 00:18:28 +0300

    update 1.40

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useState} from "react";
-import {useMemo} from "react";
 import "./App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: "r 1", body: "a"},
-        {id: 2, title: "er 2", body: "b"},
-        {id: 3, title: "ret 3", body: "c"},
-        {id: 4, title: "g 4", body: "h"},
-        {id: 5, title: "r 5", body: "t"}]
-    );
+    const [posts, setPosts] = useState("");
+    const [filter, setFilter] = useState({sort: "", query: ""});
+    const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-    const [filter, setFilter] = useState({sort: "", query: ""})
-    const [modal, setModal] = useState(false)
-
-    const sortedPosts = useMemo(() => {
-        console.log("Функция отработала")
-        if (filter.sort){
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort ]))
-        }
-        return posts;
-    }, [filter.sort, posts])
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts])
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
+    }
+
+    async function fetchPosts() {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+        setPosts(response.data)
     }
 
     const removePost = (post) => {
