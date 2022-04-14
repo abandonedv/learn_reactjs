4e4d6aa10b6163dfff827d2e8d582e6e15e087fe
Вадим Ильиных <vadimiiofficial@mail.ru>
2022-04-15 00:33:49 +0300

    little fixes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import axios from "axios";
 
 function App() {
 
-    const [posts, setPosts] = useState("");
+    const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: "", query: ""});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
